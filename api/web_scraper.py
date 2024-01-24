819f2dd6af6df81c2bd6eec2016f74c2e75312f9
graua-git <graua@oregonstate.edu>
2024-01-23 19:05:48 -0800

    HTTP GET Requests for Jooble and USA Jobs

--- api/web_scraper.py
+++ api/web_scraper.py
@@ -1,30 +1,75 @@
 import http.client 
-from pprint import pprint
 import requests
 import json
+import os
 
-def scrape_jooble():
-    """
-    Scrapes 
-    """
-    host = 'jooble.org' 
-    key = '0653066b-0c67-4996-8502-47bc1f6f923b' 
-    connection = http.client.HTTPConnection(host)
-    #request headers 
+
+def get_jooble(keywords: str, location: str):
+    # Create Request
+    host = "jooble.org"
+    key = os.getenv("JOOBLE_KEY")
+    url = f"https://{host}/api/{key}"
     headers = {"Content-type": "application/json"} 
-    #json query 
-    body = '{ "keywords": "", "location": ""}'
-    connection.request('POST','/api/' + key, body, headers) 
-    response = connection.getresponse() 
-    data = json.loads(response.read().decode('utf-8'))
-    with open("api/jobs/jooble.json", 'w') as json_file:
-        json.dump(data, json_file, indent=2)
+    body = '{"keywords": "' + keywords + '", "location": "' + location + '"}'
 
-def get_link():
-    url = "https://jooble.org/desc/-1729057608212417843"
-    response = requests.get(url)
-    print(response.text)
+    # Send Request
+    response = requests.post(url, data=body, headers=headers)
+
+    # Parse Request
+    path = "api/jobs/jooble_response.json"
+    parse_data(response, path)
+    return
+
+
+def get_usajobs(keywords: str, location: str) -> None:
+    """
+    HTTP GET Request from USA Jobs's API
+    keywords: str | job search query
+    location: str | location search query
+    """
+    # Create Request
+    url = f'https://data.usajobs.gov/api/search?Keyword={keywords}&location={location}'
+    host = 'data.usajobs.gov'
+    user_agent = os.getenv("USAJOBS_USERAGENT")
+    auth_key = os.getenv("USAJOBS_KEY")
+    headers = {
+        'Host': host,
+        'User-Agent': user_agent,
+        'Authorization-Key': auth_key
+    }
+
+    # Send Request
+    response = requests.get(url, headers=headers)
+
+    # Parse Request
+    path = "api/jobs/usajobs_response.json"
+    parse_data(response, path)
+    return
+
+
+def parse_data(response, path: str) -> None:
+    """
+    Parses response from HTTP GET request
+    If successful, stores data into path
+    If unsuccessful, print error response
+    response: response from HTTP GET
+    path: str | path to json file to store data
+    """
+    if response.status_code == 200:
+        # If successful, store data
+        data = response.json()
+        with open(path, 'w') as json_file:
+            json.dump(data, json_file, indent=2)
+    else:
+        # If unsuccessful, print error response
+        print(f"Request failed with status code {response.status_code}")
+        print(response.text) 
+    return
+
 
 if __name__ == "__main__":
-    #scrape_jooble()
-    get_link()
+    """
+    Driver code for testing purposes
+    """
+    get_jooble("teacher", "denver")
+    get_usajobs("python", "corvallis")
