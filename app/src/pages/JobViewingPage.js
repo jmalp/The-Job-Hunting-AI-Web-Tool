5232b39cefb7cbeb92bb5620630c7d0c54f25051
graua-git <graua@oregonstate.edu>
2024-02-17 22:15:16 -0800

    Job Viewing Page queries API to get list of jobs

--- app/src/pages/JobViewingPage.js
+++ app/src/pages/JobViewingPage.js
@@ -1,7 +1,34 @@
+import JobList from "../components/JobList";
+import example_jobs from "./example_jobs.json";
+import { useState, useEffect } from 'react';
+
 export default function JobViewingPage() {
+    const url = "localhost:5000"
+    const [jobs, setJobs] = useState([]);
+
+    const loadJobs = async () => {
+        //Search Jobs
+        fetch(url + '/search', {
+            method: 'GET'
+        })
+            .then((response) => response.json())
+            .then((data) => {
+                const jobs = data;
+                setJobs(jobs);
+            })
+            .catch((error) => {
+                console.error("Error Searching Jobs: ", error);
+            });
+    }
+
+    useEffect(() => {
+        loadJobs();
+    }, []);
+
     return (
         <div>
             <h1>Job Viewing Page</h1>
+            <JobList jobs={jobs} />
         </div>
     )
 }
