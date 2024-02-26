e37a4654295a19f6910daf7f6dcb068a0e9ab1f8
graua-git <graua@oregonstate.edu>
2024-02-25 19:06:15 -0800

    imported login page

--- app/src/App.js
+++ app/src/App.js
@@ -1,6 +1,7 @@
 import {BrowserRouter as Router, Routes, Route} from "react-router-dom";
 import JobViewingPage from "./pages/JobViewingPage";
 import HomeRedirect from "./pages/HomeRedirect"
+import LoginPage from "./pages/LoginPage";
 import './App.css';
 
 function App() {
@@ -9,6 +10,7 @@ function App() {
       <div className="App">
         <Routes>
           <Route exact path="/" element={<HomeRedirect />} />
+          <Route exact path="/login" element={<LoginPage />} />
           <Route exact path="/jobs" element={<JobViewingPage />} />
         </Routes>
       </div>
---
+++ app/src/pages/LoginPage.js
@@ -0,0 +1,42 @@
+import React, { useState } from 'react';
+import './Login.css';
+import user_icon from '../assets/person.png';
+import email_icon from '../assets/email.png';
+import password_icon from '../assets/password.png';
+
+export default function LoginPage() {
+    const [action, setAction] = useState("Login");
+
+    return (
+        <div className="container">
+            <div className="header">
+            <div className="text">{action}</div>
+            <div className="underline"></div>
+            </div>
+            <div className="inputs">
+                {action==="Login"?<div></div>:<div className="input">
+                    <img src={user_icon} alt="user_icon" className="icon"/>
+                    <input type="text" placeholder="Username" className="input-field"/>
+                </div>}
+                
+                <div className="input">
+                    <img src={email_icon} alt="user_icon" className="icon"/>
+                    <input type="email" placeholder="Email" className="input-field"/>
+                </div>
+                <div className="input">
+                    <img src={password_icon} alt="user_icon" className="icon"/>
+                    <input type="password" placeholder="Passoword" className="input-field"/>
+                </div>
+            
+                </div>
+                {action==="Sign Up"?<div></div>:<div className="forgot-password">Forgot Password? <span>Click here</span></div>}
+                    {/* <input type="checkbox" className="checkbox"/>
+                    <div className="text">I agree to the Terms and Conditions</div> */}
+                
+                <div className="submit-container">
+                    <div className={action==="Login"?"submit gray":"submit"} onClick={()=>{setAction("Sign Up")}}>Sign Up</div>
+                    <div className={action==="Sign Up"?"submit gray":"submit"} onClick={()=>{setAction("Login")}}>Login</div>
+                </div>
+        </div>
+  );
+}
