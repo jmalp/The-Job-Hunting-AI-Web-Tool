09b88a4c6871e1df616bc995cc2995bcd676236b
graua-git <graua@oregonstate.edu>
2024-03-04 22:59:45 -0800

    separated connect_to_db into update_db and read_db

--- api/database/db_connection.py
+++ api/database/db_connection.py
@@ -1,9 +1,9 @@
 import psycopg2
 from database.config import load_config
 
-def connect_to_db(query: str = "SELECT * FROM users LIMIT 5;") -> list:
+def read_db(query: str = "SELECT * FROM users LIMIT 5;") -> list:
     """
-    Connect to a PostgreSQL database and execute a query.
+    Connect to a PostgreSQL database and execute a SELECT query.
 
     This function connects to a PostgreSQL database using configuration parameters 
     loaded from a configuration file. It executes a provided query and prints the 
@@ -37,7 +37,45 @@ def connect_to_db(query: str = "SELECT * FROM users LIMIT 5;") -> list:
         # Close the cursor and connection
         cur.close()
         conn.close()
+
+
+def update_db(query: str) -> str:
+    """
+    Connect to a PostgreSQL database and execute an INSERT, UPDATE, or DELETE query.
+
+    This function connects to a PostgreSQL database using configuration parameters 
+    loaded from a configuration file. It executes a provided query and prints the 
+    fetched results.
+
+    Args:
+    query (str): The SQL query to be executed.
+
+    Returns:
+    str (message): success/error message
+    """
+    # Connecting to the PostgreSQL database
+    conn = psycopg2.connect(**load_config())
+
+    # Create a cursor object
+    cur = conn.cursor()
+
+    try:
+        # Execute the query
+        cur.execute(query)
         
+        # Save query
+        conn.commit()
+
+        return "Database successfully updated"
+            
+    except Exception as e:
+        print(f"An error occurred: {e}")
+        
+    finally:
+        # Close the cursor and connection
+        cur.close()
+        conn.close()
+
 
 if __name__ == "__main__":
-    connect_to_db()
+    read_from_db()
