897bcfb73868383981a693f295ccd5dfd347260c
graua-git <graua@oregonstate.edu>
2024-03-12 12:51:12 -0700

    Organizing backend by moving hash password to its own file

---
+++ api/data/hash_password.py
@@ -0,0 +1,20 @@
+import hashlib
+
+def hash_password(password: str) -> str:
+    """
+    Deterministic password hashing function for a unique input to always generate the same result
+
+    Args:
+    password (str): unhashed password
+
+    Returns:
+    hashed_password (str)
+    """
+    salted_password = password + "=DZ3;M<B?P<K"
+
+    hashed_value = hashlib.sha256(salted_password.encode('utf-8')).hexdigest()
+    
+    return hashed_value
+
+if __name__ == "__main__":
+    print(hash_password("hashed_password5"))
