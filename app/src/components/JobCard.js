773252f2f986e856e4444f43f3b2d740a0bdd0a6
graua-git <graua@oregonstate.edu>
2024-03-13 19:44:50 -0700

    Improved job card styling

--- app/src/components/JobCard.js
+++ app/src/components/JobCard.js
@@ -11,12 +11,12 @@ export default function JobCard({job}) {
     }
 
     return (
-        <Link to={job.link}>
-            <div id="card">
-                <div id="title">{job.title} @ {job.company}</div>
-                <div id="location">{job.location}</div>
-                <div id="details">{details}</div>
-                <div id="snippet">{job.snippet}</div>
+        <Link className='job-link' to={job.link} target='_blank'>
+            <div className='card'>
+                <div className='title'>{job.title} @ {job.company}</div>
+                <div className='location'>{job.location}</div>
+                <div className='details'>{details}</div>
+                <div className='snippet'>{job.snippet}</div>
             </div>
         </Link>
     )
