a2657695cca830dc94442ce6ae78926318a3dc1b
GabbyNorth <71340905+GabbyNorth@users.noreply.github.com>
2024-04-07 15:27:56 -0400

    Complete account creation feature

--- api/data/pdf_converter.py
+++ api/data/pdf_converter.py
@@ -1,6 +1,6 @@
-import fitz
+from pdfminer.high_level import extract_text
 
-def convert_pdf_to_string(file) -> str:
+def convert_pdf_to_string(file_path):
     """
     Converts PDF file to string
 
@@ -10,16 +10,9 @@ def convert_pdf_to_string(file) -> str:
     Returns:
     text (str): PDF file in text format
     """
-    # Initialize result text and open file
-    text = ""
-    document = fitz.open(stream=file.read(), filetype="pdf")
-
-    # Iterate through PDF pages, add text to result
-    for page_num in range(document.page_count):
-        page = document[page_num]
-        text += page.get_text()
-
-    document.close()
-
-    # Replace ' to prevent errors with PostGreSQL queries
-    return text.replace("'", "")
+    try:
+        # Replace ' to prevent errors with PostGreSQL queries
+        return extract_text(file_path).replace("'", "")
+    except Exception as e:
+        print(f"Error converting PDF to string: {e}")
+        return ""
